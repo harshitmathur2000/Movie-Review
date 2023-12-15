2bda397d099bd03488a5904cd9952b8733ca9f82
harshitmathur2000 <74778498+harshitmathur2000@users.noreply.github.com>
2023-12-15 21:06:40 +0530

    Add files via upload

---
+++ movie-review-app/README.md
@@ -0,0 +1,2 @@
+# Movie-review-app
+A movie review platform made using Java SpringBoot Backend and React js Frontend
---
+++ movie-review-app/movies/src/test/java/dev/anurag/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anurag.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
